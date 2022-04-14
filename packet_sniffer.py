268141f10e435e6902671ccaf47ec61fbed99e6e
Mario Zelaya <mariozelaya23@gmail.com>
2022-04-14 16:13:50 -0500

    fixing issues with string values

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -14,17 +14,17 @@ def get_url(packet):
 
 def get_login_info(packet):
     if packet.haslayer(scapy.Raw):
-        load = packet[scapy.Raw].load
+        load = str(packet[scapy.Raw].load)
         keywords = ["username", "user", "login", "pass", "password"]
         for keyword in keywords:
-            if keyword in str(load):
+            if keyword in load:
                 return load
 
 
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
         url = get_url(packet)
-        print("[+] HTTP Request >> " + str(url))
+        print("[+] HTTP Request >> " + url.decode())
 
         login_info = get_login_info(packet)
         if login_info:
